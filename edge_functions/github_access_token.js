96bbe8d86e160fa46094d04d9807b8b253c70b94
Dedicatus545 <1607611087@qq.com>
2022-10-22 11:24:20 +0800

    fix: 修复函数错误

--- edge_functions/github_access_token.js
+++ edge_functions/github_access_token.js
@@ -4,14 +4,22 @@
  * @param {*} context
  */
 module.exports = async function (request) {
-  const reqBody = request.body;
-  const res = await fetch("https://github.com/login/oauth/access_token", {
-    method: "post",
-    body: reqBody,
-  });
-  const params = new URLSearchParams(await res.text());
-  return Array.from(params.entries()).reduce((obj, [key, value]) => {
-    obj[key] = value;
-    return obj;
-  }, {});
+  try {
+    const reqBody = request.body;
+    const res = await fetch("https://github.com/login/oauth/access_token", {
+      method: "post",
+      body: reqBody,
+    });
+    const params = new URLSearchParams(await res.text());
+    return Array.from(params.entries()).reduce((obj, [key, value]) => {
+      obj[key] = value;
+      return obj;
+    }, {});
+  } catch (e) {
+    console.error(e);
+  }
+  return {
+    code: 500,
+    msg: "netlify proxy error",
+  };
 };
