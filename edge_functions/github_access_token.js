6c7538769d7b60dedb612d2c1021d745018c9cdc
Dedicatus545 <1607611087@qq.com>
2022-10-22 11:28:20 +0800

    test: 测试 edge 函数

--- edge_functions/github_access_token.js
+++ edge_functions/github_access_token.js
@@ -4,20 +4,7 @@
  * @param {*} context
  */
 module.exports = async function (request) {
-  try {
-    const reqBody = request.body;
-    const res = await fetch("https://github.com/login/oauth/access_token", {
-      method: "post",
-      body: reqBody,
-    });
-    const params = new URLSearchParams(await res.text());
-    return Array.from(params.entries()).reduce((obj, [key, value]) => {
-      obj[key] = value;
-      return obj;
-    }, {});
-  } catch (e) {
-    console.error(e);
-  }
+  console.log(request);
   return {
     code: 500,
     msg: "netlify proxy error",
