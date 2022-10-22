96741fedd83177db3a7f4c301bb8fc524df7b3ec
Dedicatus545 <1607611087@qq.com>
2022-10-22 11:54:50 +0800

    test: 测试 edge 函数

--- edge_functions/github_access_token.js
+++ edge_functions/github_access_token.js
@@ -4,23 +4,34 @@
  * @param {*} context
  */
 export default async function (request) {
-  try {
-    const reqBody = request.body;
-    const res = await fetch("https://github.com/login/oauth/access_token", {
-      method: "post",
-      body: reqBody,
-    });
-    const params = new URLSearchParams(await res.text());
-    return new Response(
-      JSON.stringify(
-        Array.from(params.entries()).reduce((obj, [key, value]) => {
-          obj[key] = value;
-          return obj;
-        }, {})
-      )
-    );
-  } catch (e) {
-    console.error(e);
-    throw e;
+  if (request.method === "OPTIONS") {
+    const resp = new Response();
+    resp.headers.set("Access-Control-Allow-Origin", "*");
+    resp.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+    return;
   }
+  if (request.method === "POST") {
+    try {
+      const reqBody = request.body;
+      const res = await fetch("https://github.com/login/oauth/access_token", {
+        method: "POST",
+        body: reqBody,
+      });
+      const params = new URLSearchParams(await res.text());
+      const resp = new Response(
+        JSON.stringify(
+          Array.from(params.entries()).reduce((obj, [key, value]) => {
+            obj[key] = value;
+            return obj;
+          }, {})
+        )
+      );
+      resp.headers.set("");
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+
+  return new Response("a cors proxy by netlify!");
 }
