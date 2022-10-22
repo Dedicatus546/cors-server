5a4ca5527f5288cc125481b3dedc825111e1effb
Dedicatus545 <1607611087@qq.com>
2022-10-22 12:20:12 +0800

    test: 测试 edge 函数

--- edge_functions/github_access_token.js
+++ edge_functions/github_access_token.js
@@ -5,19 +5,25 @@
  */
 export default async function (request) {
   if (request.method === "OPTIONS") {
+    // 预检请求
     const resp = new Response();
     resp.headers.set("Access-Control-Allow-Origin", "*");
     resp.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+    resp.headers.set("Allow", "POST");
+    resp.status = 204;
     return resp;
   }
   if (request.method === "POST") {
     try {
-      const reqBody = request.body;
+      const reqBody = await request.text();
+      console.log("request body: ", reqBody);
       const res = await fetch("https://github.com/login/oauth/access_token", {
         method: "POST",
         body: reqBody,
       });
-      const params = new URLSearchParams(await res.text());
+      const text = await res.text();
+      console.log("github api res: ", text);
+      const params = new URLSearchParams(text);
       const resp = new Response(
         JSON.stringify(
           Array.from(params.entries()).reduce((obj, [key, value]) => {
